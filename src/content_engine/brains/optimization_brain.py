958f442c5bea94dd26d8308538faf4329f9a4d05
brokedaddy01-lab <brokedaddy01@gmail.com>
2026-08-08 15:24:42 -0400

    refactor: simplify OptimizationBrain manager forwarding

--- src/content_engine/brains/optimization_brain.py
+++ src/content_engine/brains/optimization_brain.py
@@ -13,25 +13,13 @@ from content_engine.agents.optimization.optimization_manager import (
 
 class OptimizationBrain(BaseBrain):
 
-
     manager_class = OptimizationManager
 
-
-
-    ##################################################
-    # FULL OPTIMIZATION
-    ##################################################
-
     def optimize(
-
         self,
-
         content,
-
         platform=None,
-
         topic=None
-
     ):
 
         if isinstance(
@@ -42,66 +30,13 @@ class OptimizationBrain(BaseBrain):
             context = content
 
             return self.manager.optimize(
-
                 context.content,
-
                 context.platform,
-
                 context.topic
-
             )
 
-
         return self.manager.optimize(
-
             content,
-
             platform,
-
             topic
-
-        )
-
-
-
-    ##################################################
-    # CTA ONLY
-    ##################################################
-
-    def generate_cta(
-
-        self,
-
-        platform
-
-    ):
-
-        return self.manager.generate_cta(
-
-            platform
-
-        )
-
-
-
-    ##################################################
-    # HASHTAGS ONLY
-    ##################################################
-
-    def generate_hashtags(
-
-        self,
-
-        topic,
-
-        platform
-
-    ):
-
-        return self.manager.generate_hashtags(
-
-            topic,
-
-            platform
-
         )
