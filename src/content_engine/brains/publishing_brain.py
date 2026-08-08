d06f3eee87925791fc8db5c313b70ac1c2a8b617
brokedaddy01-lab <brokedaddy01@gmail.com>
2026-08-08 15:27:49 -0400

    refactor: simplify PublishingBrain manager forwarding

--- src/content_engine/brains/publishing_brain.py
+++ src/content_engine/brains/publishing_brain.py
@@ -13,112 +13,27 @@ from content_engine.agents.publishing.publishing_manager import (
 
 class PublishingBrain(BaseBrain):
 
-
     manager_class = PublishingManager
 
-
-
-    ##################################################
-    # CAMPAIGN CREATION
-    ##################################################
-
-    def create_campaign(
-
-        self,
-
-        data
-
-    ):
-
-        return self.manager.create_campaign(
-
-            data
-
-        )
-
-
-
-    ##################################################
-    # PUBLISH CONTENT
-    ##################################################
-
     def publish(
-
         self,
-
         content,
-
         platform=None
-
     ):
 
         if isinstance(
-
             content,
-
             ContentContext
-
         ):
 
             context = content
 
             return self.manager.publish(
-
                 context.content,
-
                 context.platform
-
             )
 
-
         return self.manager.publish(
-
             content,
-
             platform
-
-        )
-
-
-
-    ##################################################
-    # SCHEDULING
-    ##################################################
-
-    def schedule(
-
-        self,
-
-        content,
-
-        platform,
-
-        date
-
-    ):
-
-        return self.manager.schedule(
-
-            content,
-
-            platform,
-
-            date
-
-        )
-
-
-
-    def best_time(
-
-        self,
-
-        platform
-
-    ):
-
-        return self.manager.best_time(
-
-            platform
-
         )
