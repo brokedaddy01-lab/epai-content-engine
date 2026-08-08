6ab2c542e928fc34a5a55986843bfb9be7dfe219
brokedaddy01-lab <brokedaddy01@gmail.com>
2026-08-08 15:20:56 -0400

    refactor: simplify IntelligenceBrain manager forwarding

--- src/content_engine/brains/intelligence_brain.py
+++ src/content_engine/brains/intelligence_brain.py
@@ -13,35 +13,22 @@ from content_engine.agents.intelligence.content_memory_manager import (
 
 class IntelligenceBrain(BaseBrain):
 
-
     manager_class = IntelligenceManager
-
-
 
     def create_manager(self):
 
         return self.manager_class(
-
             ContentMemoryManager()
-
         )
 
-
-
     def __init__(
-
         self,
-
         manager=None
-
     ):
 
         super().__init__(
-
             manager
-
         )
-
 
         #
         # Compatibility aliases
@@ -52,203 +39,37 @@ class IntelligenceBrain(BaseBrain):
         #
 
         self.content_memory = (
-
             self.manager.content_memory
-
         )
-
 
         self.memory = (
-
             self.manager.memory
-
         )
-
 
         self.feedback = (
-
             self.manager.feedback
-
         )
-
 
         self.feedback_loop = (
-
             self.manager.feedback
-
         )
-
 
         self.performance = (
-
             self.manager.performance
-
         )
-
 
         self.performance_learning = (
-
             self.manager.learning
-
         )
-
 
         self.learning = (
-
             self.manager.learning
-
         )
-
 
         self.knowledge = (
-
             self.manager.knowledge
-
         )
-
 
         self.clusters = (
-
             self.manager.knowledge
-
-        )
-
-
-
-    ##################################################
-    # MEMORY
-    ##################################################
-
-    def remember_content(
-
-        self,
-
-        hook,
-
-        topic,
-
-        hashtags,
-
-        cta,
-
-        platform,
-
-        score
-
-    ):
-
-        return self.manager.remember_content(
-
-            hook,
-
-            topic,
-
-            hashtags,
-
-            cta,
-
-            platform,
-
-            score
-
-        )
-
-
-
-    def remember_failure(
-
-        self,
-
-        hook
-
-    ):
-
-        return self.manager.remember_failure(
-
-            hook
-
-        )
-
-
-
-    def prompt_context(
-
-        self
-
-    ):
-
-        return self.manager.prompt_context()
-
-
-
-    ##################################################
-    # FEEDBACK
-    ##################################################
-
-    def learn(
-
-        self,
-
-        review,
-
-        row
-
-    ):
-
-        return self.manager.learn(
-
-            review,
-
-            row
-
-        )
-
-
-
-    ##################################################
-    # KNOWLEDGE
-    ##################################################
-
-    def search(
-
-        self,
-
-        query
-
-    ):
-
-        return self.manager.search(
-
-            query
-
-        )
-
-
-
-    ##################################################
-    # PERFORMANCE
-    ##################################################
-
-    def performance_summary(
-
-        self
-
-    ):
-
-        return self.manager.performance_summary()
-
-
-
-    def learn_from_performance(
-
-        self,
-
-        data
-
-    ):
-
-        return self.manager.learn_from_performance(
-
-            data
-
         )
